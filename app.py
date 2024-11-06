65afba5231ed4b4bb334bf7bbe923f1710d46745
Douglas Henrique Rasse <projetointegradorunivesp2024@gmail.com>
2024-11-05 21:52:36 -0300

    Simplificação e ajuste do código Flask

--- app.py
+++ app.py
@@ -12,14 +12,15 @@ import logging
 app = Flask(__name__, template_folder='./templates')
 app.secret_key = 'UNIVESP'
 
+# Configuração do formato de data
 @app.template_filter('datetimeformat')
 def datetimeformat(value, format='%d-%m-%Y'):
     if value:
         return datetime.strptime(value, '%Y-%m-%d').strftime(format)
     return value
 
-# Configuração da URI do MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/controle_estoque'
+# Configuração da URI do MySQL (substitua com a URL correta do Render, se aplicável)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:123456@localhost/controle_estoque')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializa o banco de dados SQLAlchemy e Flask-Migrate
@@ -28,24 +29,16 @@ migrate = Migrate(app, db)
 
 # Configuração do Firebase
 def get_cred_file_path():
-    if getattr(sys, 'frozen', False):
-        application_path = sys._MEIPASS
-    else:
-        application_path = os.path.dirname(os.path.abspath(__file__))
-    cred_file_name = 'controle-de-mercadoria-firebase-adminsdk-z1zlf-036e096d97.json'
-    return os.path.join(application_path, cred_file_name)
+    application_path = sys._MEIPASS if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(application_path, 'controle-de-mercadoria-firebase-adminsdk-z1zlf-036e096d97.json')
 
 cred_path = get_cred_file_path()
-print(f"Path to credential file: {cred_path}")
-
 if os.path.isfile(cred_path):
-    print("File exists.")
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred)
+    firestore_db = firestore.client()
 else:
-    print("File does not exist.")
-
-cred = credentials.Certificate(cred_path)
-firebase_admin.initialize_app(cred)
-firestore_db = firestore.client()
+    print("Firebase credential file not found.")
 
 # Definição dos modelos de banco de dados
 class Produto(db.Model):
@@ -64,9 +57,7 @@ class Produto(db.Model):
     def dias_ate_vencimento(self):
         if self.data_validade:
             data_validade_dt = datetime.strptime(self.data_validade, '%Y-%m-%d').date()
-            hoje = datetime.now().date()
-            dias_ate_vencimento = (data_validade_dt - hoje).days
-            return dias_ate_vencimento
+            return (data_validade_dt - datetime.now().date()).days
         return None
 
 # Variável de controle para criação de tabelas
@@ -79,6 +70,7 @@ def create_tables():
         db.create_all()
         tables_created = True
 
+# Rota principal e outras rotas de operação
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -93,18 +85,10 @@ def home():
 
 @app.route('/estoque', methods=['GET', 'POST'])
 def estoque():
-    filtro_nome = request.args.get('filtro_nome', '')
-    filtro_codigo = request.args.get('filtro_codigo', '')
-    filtro_marca = request.args.get('filtro_marca', '')
-
     filtros = []
-    if filtro_nome:
-        filtros.append(Produto.nome.like(f'%{filtro_nome}%'))
-    if filtro_codigo:
-        filtros.append(Produto.codigo.like(f'%{filtro_codigo}%'))
-    if filtro_marca:
-        filtros.append(Produto.marca.like(f'%{filtro_marca}%'))
-
+    for filtro, valor in {'nome': request.args.get('filtro_nome', ''), 'codigo': request.args.get('filtro_codigo', ''), 'marca': request.args.get('filtro_marca', '')}.items():
+        if valor:
+            filtros.append(getattr(Produto, filtro).like(f'%{valor}%'))
     produtos = Produto.query.filter(*filtros).all()
     return render_template('estoque.html', produtos=produtos)
 
@@ -114,49 +98,30 @@ def excluir_produto(id):
     if produto:
         db.session.delete(produto)
         db.session.commit()
-        flash('Produto excluído com sucesso.', 'success')
         return jsonify({'success': True}), 200
-    else:
-        return jsonify({'success': False}), 404
+    return jsonify({'success': False}), 404
 
 @app.route('/cadastro-mercadoria', methods=['GET', 'POST'])
 def cadastro_mercadoria():
     if request.method == 'POST':
-        nome_produto = request.form.get('nomeProduto')
-        categoria_produto = request.form.get('categoria')  # Corrigido para categoria
-        informacoes_produto = request.form.get('informacoesProduto')
-        codigo = request.form.get('codigo')
-        custo = request.form.get('custo')
-        quantidade = request.form.get('quantidadeProduto')
-        data_cadastro = request.form.get('dataCadastro')
-        data_validade = request.form.get('dataValidade')
-
-        novo_produto = Produto(
-            nome=nome_produto,
-            marca=categoria_produto,  # Armazena a categoria em 'marca'
-            informacoes=informacoes_produto,
-            custo=custo,
-            quantidade=quantidade,
-            data_inclusao=data_cadastro,
-            data_validade=data_validade,
-            codigo=codigo
-        )
+        dados_produto = {
+            'nome': request.form.get('nomeProduto'),
+            'marca': request.form.get('categoria'),
+            'informacoes': request.form.get('informacoesProduto'),
+            'codigo': request.form.get('codigo'),
+            'custo': request.form.get('custo'),
+            'quantidade': request.form.get('quantidadeProduto'),
+            'data_inclusao': request.form.get('dataCadastro'),
+            'data_validade': request.form.get('dataValidade')
+        }
+        novo_produto = Produto(**dados_produto)
         db.session.add(novo_produto)
         db.session.commit()
-        flash('Produto cadastrado com sucesso.', 'success')
 
+        # Firebase storage
         try:
             produto_ref = firestore_db.collection('produtos').document()
-            produto_ref.set({
-                'nome': nome_produto,
-                'categoria': categoria_produto,
-                'informacoes': informacoes_produto,
-                'codigo': codigo,
-                'custo': custo,
-                'quantidade': quantidade,
-                'data_inclusao': data_cadastro,
-                'data_validade': data_validade
-            })
+            produto_ref.set(dados_produto)
             flash('Produto cadastrado com sucesso no Firebase.', 'success')
         except Exception as e:
             app.logger.error(f"Erro ao salvar no Firestore: {e}")
@@ -175,20 +140,16 @@ def lista_produtos_proximo_vencimento():
 @app.route('/detalhes-produto/<int:id>', methods=['GET'])
 def detalhes_produto(id):
     produto = Produto.query.get(id)
-    if produto:
-        detalhes = {
-            "id": produto.id,
-            "nome": produto.nome,
-            "categoria": produto.marca,  # Ajustado para mostrar a categoria
-            "informacoes": produto.informacoes,
-            "quantidade": produto.quantidade,
-            "custo": produto.custo,
-            "data_inclusao": produto.data_inclusao,
-            "data_validade": produto.data_validade
-        }
-        return jsonify(detalhes), 200
-    else:
-        return jsonify({"error": "Produto não encontrado"}), 404
+    return jsonify({
+        "id": produto.id,
+        "nome": produto.nome,
+        "categoria": produto.marca,
+        "informacoes": produto.informacoes,
+        "quantidade": produto.quantidade,
+        "custo": produto.custo,
+        "data_inclusao": produto.data_inclusao,
+        "data_validade": produto.data_validade
+    }) if produto else jsonify({"error": "Produto não encontrado"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
